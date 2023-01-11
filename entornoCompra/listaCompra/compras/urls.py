f31910bb0993be76c3ec40e09e00e09baf6719d0
Perla Celaya <zx21student047@colegiovalledelmiro.es>
2023-01-11 13:42:25 +0100

    Creada lista de la compra, comprado y añadir de nuevo

--- entornoCompra/listaCompra/compras/urls.py
+++ entornoCompra/listaCompra/compras/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.compras, name='compras'),
+    path('nuevo',views.nuevo,name='nuevo'),
+    path('comprado/<int:identificador>', views.comprado, name='comprado'),
+    path('anadir/<int:identificador>',views.anadir,name='anadir')
 ]
--- entornoCompra/listaCompra/compras/views.py
+++ entornoCompra/listaCompra/compras/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from .models import Compras
 from django.template import loader
+from django.urls import reverse
 # Create your views here.
 def compras(request):
     lc = Compras.objects.all()
@@ -10,3 +11,24 @@ def compras(request):
     }
     plantilla = loader.get_template('compras.html')
     return HttpResponse(plantilla.render(context,request))
+
+def nuevo(request):
+    prod = request.GET['producto'] #Es un objeto que recupera lo que hemos puesto en el campo nuevoElemento del form
+    cant = request.GET['cantidad']
+    unid = request.GET['unidad']
+    compra = Compras(producto=prod,cantidad=cant, unidad=unid,comprada = False)
+    compra.save()
+    
+    return HttpResponseRedirect(reverse('compras'))
+
+def comprado (request, identificador):
+    cmpr = Compras.objects.get(id=identificador)
+    cmpr.comprada = True
+    cmpr.save()
+    return HttpResponseRedirect(reverse('compras'))
+
+def anadir (request, identificador):
+    cmpr = Compras.objects.get(id=identificador)
+    cmpr.comprada = False
+    cmpr.save()
+    return HttpResponseRedirect(reverse('compras'))
